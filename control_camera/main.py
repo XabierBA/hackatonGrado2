c87244e3f9b923cd3dc53186bfb0588a4e1a29e9
XabierBA <xabitic1@gmail.com>
2025-04-05 13:11:15 +0200

    v1logs

--- control_camera/main.py
+++ control_camera/main.py
@@ -1,7 +1,7 @@
 import os
 import json
 from utils import check_camera
-
+from datetime import datetime
 
 # Cargamos el archivo JSON que contiene las rutas
 with open('path.json','r') as file:
@@ -14,7 +14,23 @@ with open('cam_id.json', 'r') as file:
 usuario_actual = os.getenv("USER")
 
 ruta_template = data["paths"][0]
-
 id_template = cam_data["sup_cam"][0]
-          
-print(check_camera(usuario_actual, id_template, cam_data, data))
+
+# Verificamos si la carpeta 'logs' existe, si no, la creamos
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+# Obtenemos el timestamp actual
+timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+# Ejecutamos la función check_camera
+result = check_camera(usuario_actual, id_template, cam_data, data)
+
+# Definimos el archivo de log
+log_file = 'logs/check_camera_log.txt'
+
+# Escribimos el resultado con timestamp en el archivo de log
+with open(log_file, 'a') as log:
+    log.write(f"{timestamp} - {result}\n")
+    
+print(result)  # También puedes imprimir el resultado en la consola si es necesario
