65731cd03e2721c98c46a19f7e118a49776b09a0
XabierBA <xabitic1@gmail.com>
2025-04-05 11:26:41 +0200

    v3.5 Resolved

--- control_camera/main.py
+++ control_camera/main.py
@@ -1,16 +1,14 @@
 import os
 import json
 from pathlib import Path
-with open('path.json', 'r') as file:
+with open('path.json','r') as file:
     data = json.load(file)
 
 usuario_actual = os.getenv("USER")
 
 id_camara = "9C33-6BBD"
 
-path = Path(f"/media/{usuario_actual}/{id_camara}/DCIM")
-
-for ruta_template in data["path"]:
+for ruta_template in data["paths"]:
     path = ruta_template.format(usuario=usuario_actual, id_camara=id_camara)
 
     if Path(path).is_dir():
