60cbe9c7a1e355e61ff376260e5906eba9656199
XabierBA <xabitic1@gmail.com>
2025-04-05 12:14:08 +0200

    v4 camera found getted

--- control_camera/main.py
+++ control_camera/main.py
@@ -1,12 +1,18 @@
 import os
 import json
 from pathlib import Path
+
+# Cargamos el archivo JSON que contiene las rutas
 with open('path.json','r') as file:
     data = json.load(file)
 
+# Cargamos el archivo JSON con los id de cámaras
+with open('cam_id.json', 'r') as file:
+    cam_data = json.load(file)
+
 usuario_actual = os.getenv("USER")
 
-id_camara = "9C33-6BBD"
+ruta_template = data["paths"][0]
 
 for ruta_template in data["paths"]:
     path = ruta_template.format(usuario=usuario_actual, id_camara=id_camara)
